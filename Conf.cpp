db0fff82d29598353dfb6d5b8bb7c6126bf1fad2
NIshant Sabharwal <nishsab@ip-192-168-1-4.us-west-2.compute.internal>
2020-10-22 20:16:27 -0700

    Created conf class and class to crawl through directory and list files.

---
+++ Conf.cpp
@@ -0,0 +1,35 @@
+//
+// Created by NIshant Sabharwal on 10/21/20.
+//
+
+#include <fstream>
+#include <iostream>
+#include "Conf.h"
+#include "Utils.h"
+#include <vector>
+#include <unordered_map>
+
+Conf::Conf(string filename) {
+    ifstream fin(filename);
+    string line;
+    unordered_map<string, string> configs;
+    while (getline(fin, line)) {
+        vector<string> tokens = Utils::split(line, '=');
+        if (tokens.size() != 2) {
+            Utils::errf("Invalid config line: %s\n", line.c_str());
+            throw -1;
+        }
+        configs[tokens[0]] = tokens[1];
+    }
+
+    heartbeatWindow = atoi(getString(configs, "heartbeatWindow").c_str());
+    outboundDirectory = getString(configs, "outboundDirectory");
+    inboundDirectory = getString(configs, "inboundDirectory");
+}
+
+string Conf::getString(unordered_map<string, string> configs, string key) {
+    Utils::errf("Parsing: %s\n", key.c_str());
+    string val = configs[key];
+    Utils::errf("Setting %s to %s\n", key.c_str(), val.c_str());
+    return val;
+}
---
+++ Conf.h
@@ -0,0 +1,23 @@
+//
+// Created by NIshant Sabharwal on 10/21/20.
+//
+
+#ifndef NDN_DROP_CONF_H
+#define NDN_DROP_CONF_H
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+class Conf {
+public:
+    Conf(string filename);
+    int heartbeatWindow;
+    string outboundDirectory;
+    string inboundDirectory;
+private:
+    string getString(unordered_map<string, string> configs, string key);
+};
+
+
+#endif //NDN_DROP_CONF_H
